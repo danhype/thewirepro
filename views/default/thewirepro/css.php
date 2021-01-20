e1c6660761aabad8b05f69e9dc265514b67a9fb1
Dan d'Auge <76149200+danhype@users.noreply.github.com>
2021-01-20 16:13:02 +0100

    Add files via upload

---
+++ views/default/thewirepro/css.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The Wire CSS
+ */
+
+?>
+/********************************
+The Wire Pro
+*********************************/
+.embera-embed-responsive {
+  position: relative;
+  display: block;
+  width: 100%;
+  padding: 0;
+  overflow: hidden;
+  padding-bottom: 50%;
+}
+
+.embera-embed-responsive-item {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+
+
+.elgg-item-object-thewire  {
+  /* Add shadows to create the "card" effect */
+  margin-top:8px;
+  box-shadow: 0 1px 2px 0 rgba(0,0,0,0.2);
+}
+
+
+.thewire-container {
+  padding: 2px 16px;
+}
+
+.elgg-menu-social{
+	font-size:1.1rem;
+}
+
+.elgg-menu-hz {
+	margin-left:5px;
+}
