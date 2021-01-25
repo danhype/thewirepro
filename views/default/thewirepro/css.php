8e96900b41f754300f3da52bc6e71918d8d3f9bd
Dan d'Auge <76149200+danhype@users.noreply.github.com>
2021-01-25 15:05:39 +0100

    Update css news attributs

--- views/default/thewirepro/css.php
+++ views/default/thewirepro/css.php
@@ -45,3 +45,26 @@ The Wire Pro
 .elgg-menu-hz {
 	margin-left:5px;
 }
+
+.plusvideos,
+.plusblogs {
+    color: #fff;
+    background: #019cf6;
+    border-radius: 3px;
+    padding: 3px;
+    transition: background-color .5s,color .5s;
+    line-height: 1.75rem;
+    padding: .325rem 1rem;
+    text-decoration: none;
+    float: right;
+    margin-left: 1px;
+    margin-right: 1px;
+}
+
+.plusvideos:hover,
+.plusblogs:hover {
+    background: #9206c1;
+    color: #fff;
+    text-decoration: none;
+}
+
